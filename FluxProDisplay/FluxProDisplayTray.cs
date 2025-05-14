077cbc98bbe34e66f30a748ee4cc6e5da4700cb4
Brandon A <bantillon@pm.me>
2025-05-14 14:59:42 -0700

    swapped out ancient openhardwaremonitor for librehardwaremonitor

--- FluxProDisplay/FluxProDisplayTray.cs
+++ FluxProDisplay/FluxProDisplayTray.cs
@@ -6,50 +6,62 @@ namespace FluxProDisplay;
 public partial class FluxProDisplayTray : Form
 {
     public HidDevice? Device;
+    public HardwareMonitor Monitor;
 
+    // offload to appsettings
     private const int PollingInterval = 1;
-
-    const int vendorId = 0x2022;  // Replace with your device's VID
-    const int productId = 0x0522; // Replace with your device's PID
+    const int VendorId = 0x2022;
+    const int ProductId = 0x0522;
 
     public FluxProDisplayTray()
     {
         InitializeComponent();
 
+        Monitor = new HardwareMonitor();
         _ = WriteToDisplay();
     }
 
     private async Task WriteToDisplay()
     {
+        // interval of 1 sec
         var timer = new PeriodicTimer(TimeSpan.FromSeconds(PollingInterval));
+
+        var device = HidDevices.Enumerate(VendorId, ProductId).FirstOrDefault();
 
         do
         {
-            var device = HidDevices.Enumerate(vendorId, productId).FirstOrDefault();
-            int reportLength = device.Capabilities.OutputReportByteLength;
-
-            // Example payload (report)
-            // The first byte is the Report ID (usually 0x00)
-            // 85, 170, 1, 1, 6, 2, 4, 0, 1, 6, 0, 20
-            byte[] payload = new byte[reportLength]; // Customize as needed
-
-            payload[0] = 0;
-            payload[1] = 85;
-            payload[2] = 170;
-            payload[3] = 1;
-            payload[4] = 1;
-            payload[5] = 6;
-            payload[6] = 2;
-            payload[7] = 4;
-            payload[8] = 0;
-            payload[9] = 1;
-            payload[10] = 6;
-            payload[11] = 0;
-            payload[12] = 20;
-
-            device.Write(payload);
-
-            device.CloseDevice();
+            device?.Write(GeneratePayload(device));
         } while (await timer.WaitForNextTickAsync());
+    }
+
+    /// <summary>
+    /// generates the encoded payload to the display
+    /// </summary>
+    /// <param name="device"></param>
+    /// <returns></returns>
+    private byte[] GeneratePayload(HidDevice device)
+    {
+        int reportLength = device.Capabilities.OutputReportByteLength;
+        byte[] payload = new byte[reportLength];
+
+        // reporting number, and other information needed to send to the display
+        payload[0] = 0;
+        payload[1] = 85;
+        payload[2] = 170;
+        payload[3] = 1;
+        payload[4] = 1;
+        payload[5] = 6;
+
+        // representation of numbers
+        Console.WriteLine(Monitor.GetCpuTemperature());
+        payload[6] = 2;
+        payload[7] = 4;
+        payload[8] = 0;
+        payload[9] = 1;
+        payload[10] = 6;
+        payload[11] = 0;
+        payload[12] = 20;
+
+        return payload;
     }
 }
---
+++ FluxProDisplay/HardwareMonitor.cs
@@ -0,0 +1,62 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+namespace FluxProDisplay;
+
+public class HardwareMonitor
+{
+    public Computer Computer;
+
+    public HardwareMonitor()
+    {
+        // change this in the future to be configurable.
+        Computer = new Computer()
+        {
+            IsCpuEnabled = true,
+            IsGpuEnabled = true
+        };
+
+        Computer.Open();
+        Computer.Accept(new UpdateVisitor());
+    }
+
+    public float? GetCpuTemperature()
+    {
+        foreach (IHardware hardware in Computer.Hardware)
+        {
+            Console.WriteLine("Hardware: {0}", hardware.Name);
+
+            foreach (IHardware subhardware in hardware.SubHardware)
+            {
+                Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+
+                foreach (ISensor sensor in subhardware.Sensors)
+                {
+                    Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+                }
+            }
+
+            foreach (ISensor sensor in hardware.Sensors)
+            {
+                Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+            }
+        }
+
+        foreach (var hardware in Computer.Hardware)
+        {
+            if (hardware.HardwareType == HardwareType.Cpu)
+            {
+                hardware.Update();
+
+                foreach (var sensor in hardware.Sensors)
+                {
+                    if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("Core"))
+                    {
+                        return sensor.Value;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
