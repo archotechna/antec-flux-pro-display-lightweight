5486df93c34cce50cedfaab95b29dd1585d64529
Brandon A <bantillon@pm.me>
2025-05-17 18:39:48 -0700

    added a mutex to stop the app from running more than once

--- FluxProDisplay/Program.cs
+++ FluxProDisplay/Program.cs
@@ -2,15 +2,32 @@ namespace FluxProDisplay;
 
 static class Program
 {
+    // name of the mutex
+    private static readonly string MutexName = "FluxProDisplay_SingleInstance_Mutex";
+
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new FluxProDisplayTray());
+        bool createdNew;
+
+        // use a mutex to only ever allow 1 instance of FluxProDisplay
+        using (new Mutex(true, MutexName, out createdNew))
+        {
+            if (!createdNew)
+            {
+                MessageBox.Show(
+                    "Another instance of FluxProDisplay is already running.",
+                    "Instance Running",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FluxProDisplayTray());
+        }
     }
 }
